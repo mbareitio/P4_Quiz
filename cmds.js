b7179e501463143468137a4937f522e047eb8cb7
mbareitio <m.bareitio@alumnos.upm.es>
2018-03-11 09:32:53 +0100

    p3_quiz log 1 linea

--- cmds.js
+++ cmds.js
@@ -218,9 +218,9 @@ exports.testCmd = (rl, id) => {
   		return makeQuestion(rl , `${colorize('Respuesta: ', 'black')}`)
   		.then(a => {
   			if(quiz.answer.toLowerCase() === a.toLowerCase().trim()){
-				log(`${colorize('La respuesta es', 'black')} ${colorize('correcta', 'green')} `);
+				console.log(`${colorize('La respuesta es', 'black')} ${colorize('correcta', 'green')}. `);
   			}else{
-				log(`${colorize('La respuesta es', 'black')} ${colorize('incorrecta', 'red')} `);
+				console.log(`${colorize('La respuesta es', 'black')} ${colorize('incorrecta', 'red')}. `);
   			}
   		})
   	})
@@ -257,9 +257,7 @@ exports.testCmd = (rl, id) => {
 	
 		const play = () => {
 			if (toBeResolved.length === 0 || toBeResolved[0] === "undefined" || typeof toBeResolved === "undefined"){
-				log(`${colorize('Ya no hay mas preguntas!', 'black')} `);
-				biglog(` Puntuación: ${score}`, 'magenta');
-
+				log(`${colorize('Ya no hay mas preguntas!', 'black')} ${colorize(`Puntuación: ${score}`, 'magenta')} `);
 			} else {
 				let i = Math.floor(Math.random() * (toBeResolved.length -1));
 				let quiz = toBeResolved[i];
@@ -272,12 +270,10 @@ exports.testCmd = (rl, id) => {
 				.then(a => {
 		  			if(quiz.answer.toLowerCase() === a.toLowerCase().trim()){
 		  				score++;
-						log(`${colorize('La respuesta es', 'black')} ${colorize('correcta', 'green')} `);
-						biglog(`+1`, 'yellow');
+						console.log(`${colorize('La respuesta es', 'black')} ${colorize('correcta', 'green')}. ${colorize('Puntuación: +1', 'magenta')}`);
 						play();
 		  			}else{
-						log(`${colorize('La respuesta es', 'black')} ${colorize('incorrecta', 'red')} `);
-						biglog(` Puntuación: ${score}`, 'magenta');
+						console.log(`${colorize('La respuesta es', 'black')} ${colorize('incorrecta', 'red')}.  ${colorize(`Puntuación: ${score}`, 'magenta')}`);
 		  			}
 	  			})
 	  			.then(() => {
